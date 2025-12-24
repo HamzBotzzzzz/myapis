4a1eb432c7cd29597e4c25b6d744d67c64e5e08b
Hamzzzzz <109900268+HamzBotzzzzz@users.noreply.github.com>
2025-12-24 21:33:06 +0700

    Add multer for handling file uploads

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-require('dotenv').config();
+const multer = require('multer');
 const fs = require('fs');
 const { LowSync } = require('lowdb');
 const { JSONFileSync } = require('lowdb/node');
